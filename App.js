8d1b7b29e168cc3684848f6d4368cc9d0ac51eb8
CHINA\h84174510 <vohoanganhktn@gmail.com>
2021-05-18 10:57:40 +0300

    add hms availability

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {StyleSheet, AppRegistry} from 'react-native'
 import React, {useEffect} from 'react';
 import { Permission, PERMISSION_TYPE } from './AppPermisson'
 import Routes from './Routes';
+import HMSAvailability, {ErrorCode} from "@hmscore/react-native-hms-availability";
 
 
  
@@ -10,9 +11,27 @@ import Routes from './Routes';
 const App =() => {
 
 
+
+  
+
+
   useEffect(() => {
+
     Permission.requestMultiple([PERMISSION_TYPE.location, PERMISSION_TYPE.backgoundLocation, PERMISSION_TYPE.activityDection])
-   
+
+    
+    const getErrorString = (errorCode = ErrorCode.HMS_CORE_APK_UNAVAILABLE) => {
+      HMSAvailability.getErrorString(errorCode)
+       .then((res) => console.log(res))
+       .catch((err) => console.log(err));
+    }
+
+    HMSAvailability.isHuaweiMobileServicesAvailable().then((res) => getErrorString(res))
+
+
+    return () => {
+      
+    }
   }, [])
 
   
